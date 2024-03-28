fdc095fd24c28de3dea528c301dd34255719e433
ckyu <dbcjdrb1997@gmail.com>
2024-03-28 13:16:50 +0900

    [Serialization]

--- src/main/kotlin/json/step2/jsonConverter.kt
+++ src/main/kotlin/json/step2/jsonConverter.kt
@@ -1,5 +1,6 @@
 package org.example.json.step2
 
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 private fun main(){
@@ -14,5 +15,14 @@ private fun main(){
 //    val user = Gson().fromJson(jsonString, User::class.java)
     val decodeUser = Json.decodeFromString<User>(jsonString)
     println(decodeUser)
+    println("--------------------")
+    val datalist = listOf(User("ck","ck@gmail.com", 18),User("kk","kk@gmail.com"))
+
+    val jsonList = Json.encodeToString(datalist)
+
+    val jsonWithDefaults = Json { encodeDefaults = true }
+    val jsonList2 = jsonWithDefaults.encodeToString(datalist)
+    println(jsonList)
+    println(jsonList2)
 
 }
